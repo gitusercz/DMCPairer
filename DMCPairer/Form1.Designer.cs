4751526c982599c804189f926d5ba10c637b809a
Attila Czibere <czibere.a@gmail.com>
2022-06-01 16:11:00 +0200

    Path to XML is hardcoded. part from that final.

--- DMCPairer/Form1.Designer.cs
+++ DMCPairer/Form1.Designer.cs
@@ -30,13 +30,20 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 114);
+            this.richTextBox1.Size = new System.Drawing.Size(776, 86);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -50,16 +57,81 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 132);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(776, 39);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Message";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current scan";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 198);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(776, 96);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "XML preview";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(12, 314);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(776, 32);
+            this.richTextBox4.TabIndex = 7;
+            this.richTextBox4.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
+            this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Wow.. So DMC Pairer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +139,12 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
     }
 }
 
--- DMCPairer/Form1.cs
+++ DMCPairer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,88 @@ namespace DMCPairer
 {
     public partial class Form1 : Form
     {
+        string PlateDMC = "";
+        string VehicleDMC = "";
+        string temp = "";
+        int int_UnitScanned = 1;
+        Boolean IsPlateDMCNext = true;
         public Form1()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+            var str_XMLfilecontent = File.ReadLines(richTextBox4.Text);
+            foreach (var line_to_be_processed in str_XMLfilecontent)
+            {
+                if (line_to_be_processed == "</Serials>")
+                {
+
+                }
+                else temp += line_to_be_processed + Environment.NewLine;
+
+            }
+            temp += richTextBox3.Text;
+            string[] str_tofile = { " " };
+            str_tofile[0] = temp + "</Serials>" ;
+            File.WriteAllLines(richTextBox4.Text, str_tofile);
+            int_UnitScanned--;
+            MessageBox.Show("XML file was updated with " +int_UnitScanned.ToString()+ " entries successfully. " + System.Environment.NewLine + "You may exit the application. ");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Welcome! Scan the plate DMC for unit 1";
+            richTextBox2.Select();
+            richTextBox4.Text = "C:\\02 ProjectServer\\DQ400_CFD_ASB\\04 Configuration Files\\Serials_DQ400.xml";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+
+
+        {
+            if (e.KeyCode == Keys.Enter)
+
+            {
+                if (IsPlateDMCNext)
+                {
+                    richTextBox1.Text = "Scan VehicleDMC for unit " + int_UnitScanned.ToString() + " !";
+                    temp = richTextBox2.Text.Replace("\n", "").Replace("\r", "");
+                    richTextBox3.Text += " <TCU MetalPlateID=\"" + temp;
+                    richTextBox3.Text += "\" VehicleConnectorID=\"";
+                    richTextBox2.Text = "";
+                    IsPlateDMCNext = false;
+                    int_UnitScanned++;
+                }
+                else
+                {
+                    richTextBox1.Text = "Scan PlateDMC for unit " + int_UnitScanned.ToString() + " !";
+                    temp = richTextBox2.Text.Replace("\n", "").Replace("\r", "");
+                    richTextBox3.Text += temp;
+                    
+                    richTextBox3.Text += "\"/>" + Environment.NewLine;
+                    richTextBox2.Text = "";
+                    IsPlateDMCNext = true;
+                    
+                }
+                    
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
